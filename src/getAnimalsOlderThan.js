2fab6e9aeab653382ed6b25deaa6721d9eef2b57
Ruham Leal <ruhamxlpro@hotmail.com>
2022-06-29 17:38:52 -0300

    Adiciona requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species.filter((Eanimal) => Eanimal.name === animal).map((elementAnimal) =>
+    elementAnimal.residents.every((e) => e.age >= age))[0];
 }
+
+console.log(getAnimalsOlderThan('lions', 1));
 
 module.exports = getAnimalsOlderThan;
