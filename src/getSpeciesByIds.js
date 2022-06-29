2a059b788b43fba7efc0ac2b432d63b5e8dad3de
Ruham Leal <ruhamxlpro@hotmail.com>
2022-06-29 17:54:53 -0300

    Adiciona mudança no Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -4,6 +4,7 @@ const { species } = data;
 
 function getSpeciesByIds(...ids) {
   if (ids.length === 0) return [];
+  if (ids.length === 1) return species.filter((element) => element.id === ids[0]);
   return species.filter((element, i) => element.id === ids[i]);
 }
 
