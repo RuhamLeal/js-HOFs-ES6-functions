8ad59dbede9a6900923fa432471241d886758cfd
Ruham Leal <ruhamxlpro@hotmail.com>
2022-06-30 03:12:44 -0300

    Adiciona requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const names = employees.filter((element) => element.managers.includes(managerId) === true);
+    return names.map((element) => `${element.firstName} ${element.lastName}`);
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
+
+/* console.log(getRelatedEmployees('4b40a139-d4dc-4f09-822d-ec25e819a5ad')); */
 
 module.exports = { isManager, getRelatedEmployees };
