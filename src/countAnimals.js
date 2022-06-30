bcfc1b70991262cd17c45cad5f0fe639844ef597
Ruham Leal <ruhamxlpro@hotmail.com>
2022-06-30 04:22:22 -0300

    Adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  } if (animal.sex === undefined) {
+    return species.find((e) => e.name === animal.specie).residents.length;
+  } return species.find((e) => e.name === animal.specie).residents.reduce((acc, curr) => {
+    if (animal.sex === curr.sex) return acc + 1;
+    return acc;
+  }, 0);
 }
 
 module.exports = countAnimals;
