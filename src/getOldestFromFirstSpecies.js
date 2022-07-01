3f02b4543b264ba46b3ef518389ab664ea02751b
Ruham Leal <ruhamxlpro@hotmail.com>
2022-07-01 02:41:00 -0300

    Adiciona requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
+const olderAge = (animals) => animals.reduce((acc, curr) => {
+  if (acc < curr.age) {
+    let soma = 0;
+    soma = curr.age - acc;
+    return acc + soma;
+  } return acc;
+}, 0);
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animalID = employees.find((e) => e.id === id).responsibleFor[0];
+  const animals = species.find((e) => e.id === animalID).residents;
+  const returnOlderAge = olderAge(animals);
+  const olderAnimal = animals.find((e) => e.age === returnOlderAge);
+  return [olderAnimal].reduce((acc, curr) => {
+    acc.push(curr.name, curr.sex, curr.age);
+    return acc;
+  }, []);
 }
 
 module.exports = getOldestFromFirstSpecies;
