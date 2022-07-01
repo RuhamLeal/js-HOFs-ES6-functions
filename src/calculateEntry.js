3679f105c6b07415c3b7669bec8a4c6b728bbe6e
Ruham Leal <ruhamxlpro@hotmail.com>
2022-07-01 01:54:01 -0300

    Adiciona requisito 9

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,30 +2,27 @@ const data = require('../data/zoo_data');
 
 const { prices } = data;
 
-const entradas = [
+/* const entradas = [
   { name: 'Lara Carvalho', age: 5 },
   { name: 'Frederico Moreira', age: 5 },
   { name: 'Pedro Henrique Carvalho', age: 5 },
   { name: 'Maria Costa', age: 18 },
   { name: 'Núbia Souza', age: 18 },
   { name: 'Carlos Nogueira', age: 50 },
-];
+]; */
 
 function countEntrants(entrants) {
   return entrants.reduce((acc, curr) => {
     if (curr.age < 18) {
       acc.child += 1;
       return acc;
-    }
-    if (curr.age >= 18 && curr.age < 50) {
+    } if (curr.age >= 18 && curr.age < 50) {
       acc.adult += 1;
       return acc;
-    }
-    if (curr.age >= 50) {
+    } if (curr.age >= 50) {
       acc.senior += 1;
       return acc;
-    }
-    return acc;
+    } return acc;
   }, { child: 0, adult: 0, senior: 0 });
 }
 
@@ -38,7 +35,5 @@ function calculateEntry(entrants) {
     return acc + som;
   }, 0);
 }
-
-console.log(calculateEntry(entradas));
 
 module.exports = { calculateEntry, countEntrants };
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,52 @@
 const data = require('../data/zoo_data');
 
+const { species, hours } = data;
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const reduceObject = {
+  Monday: { officeHour: 0, exhibition: 0 },
+  Tuesday: { officeHour: 0, exhibition: 0 },
+  Wednesday: { officeHour: 0, exhibition: 0 },
+  Thursday: { officeHour: 0, exhibition: 0 },
+  Friday: { officeHour: 0, exhibition: 0 },
+  Saturday: { officeHour: 0, exhibition: 0 },
+  Sunday: { officeHour: 0, exhibition: 0 },
+};
+
+function filterAnimals(day) {
+  return species.filter((e) => e.availability.includes(day) === true).map((e) => e.name);
+}
+
+function returnToGetSchedule2(day) {
+  return days.reduce((acc, curr) => {
+    acc[day] = {
+      exhibition: filterAnimals(day),
+      officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+    }; return acc;
+  }, {});
+}
+
+function returnToGetSchedule1() {
+  return days.reduce((acc, curr) => {
+    if (curr === 'Monday') {
+      acc[curr].exhibition = 'The zoo will be closed!';
+      acc[curr].officeHour = 'CLOSED';
+      return acc;
+    } acc[curr].exhibition = filterAnimals(curr);
+    acc[curr].officeHour = `Open from ${hours[curr].open}am until ${hours[curr].close}pm`;
+    return acc;
+  }, reduceObject);
+}
+
+const returnSchedule1 = returnToGetSchedule1();
+const mondaySchedule = { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
+const animals = species.map((e) => e.name);
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (animals.includes(scheduleTarget) === true) {
+    return species.find((animal) => animal.name === scheduleTarget).availability;
+  } if (scheduleTarget === 'Monday') return mondaySchedule;
+  if (days.includes(scheduleTarget) === true) return returnToGetSchedule2(scheduleTarget);
+  return returnSchedule1;
 }
 
 module.exports = getSchedule;
